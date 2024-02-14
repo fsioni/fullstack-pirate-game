94a7672a4b6eab46bf2c7c9af34a04ad08aa9243
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2024-02-14 18:11:17 +0100

    Add dao

---
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/dao/UserDao.java
@@ -0,0 +1,46 @@
+package fr.univlyon1.m1if.m1if13.users.dao;
+
+import fr.univlyon1.m1if.m1if13.users.models.Species;
+import fr.univlyon1.m1if.m1if13.users.models.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class UserDao implements Dao<User> {
+    private final List<User> users = new ArrayList<>();
+
+    public UserDao() {
+        users.add(new User("admin", Species.ADMIN, "admin"));
+        users.add(new User("player1", Species.PIRATE, "password1", "pirate-1.png"));
+        users.add(new User("player2", Species.PIRATE, "password2"));
+        users.add(new User("player3", Species.VILLAGEOIS, "password3", "villageois-1.png"));
+        users.add(new User("player4", Species.VILLAGEOIS, "password4"));
+    }
+
+    @Override
+    public Optional<User> get(String login) {
+        return users.stream().filter(user -> user.getLogin().equals(login)).findFirst();
+    }
+
+    @Override
+    public Set<String> getAll() {
+        return users.stream().map(User::getLogin).collect(java.util.stream.Collectors.toSet());
+    }
+
+    @Override
+    public void save(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public void update(User user, String[] params) {
+        user.setSpecies(Objects.requireNonNull(Species.valueOf(params[0])));
+        user.setPassword(Objects.requireNonNull(params[1]));
+    }
+
+    @Override
+    public void delete(User user) {
+        users.remove(user);
+    }
+}
