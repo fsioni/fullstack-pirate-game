a03d9ea4dc999959c24c9aea558b07b04cd33a08
LeXillal <julien38.b@gmail.com>
2024-02-20 16:34:17 +0100

    feat: logOut / authenticate /
    + javaDoc

--- users/src/main/java/fr/univlyon1/m1if/m1if13/users/controllers/UsersOperationsController.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/controllers/UsersOperationsController.java
@@ -14,6 +14,7 @@ public class UsersOperationsController {
 
     UserDao userDao;
 
+
     @Autowired
     public UsersOperationsController(UserDao userDao) {
         this.userDao = userDao;
@@ -22,12 +23,16 @@ public class UsersOperationsController {
     /**
      * Procédure de login utilisée par un utilisateur
      *
-     * @param login    Le login de l'utilisateur. L'utilisateur doit avoir été créé préalablement et son login doit être présent dans le DAO.
+     * @param login    Le login de l'utilisateur. L'utilisateur doit avoir été créé
+     *                 préalablement et son login doit être présent dans le DAO.
      * @param password Le password à vérifier.
-     * @return Une ResponseEntity avec le JWT dans le header "Authentication" si le login s'est bien passé, et le code de statut approprié (204, 401 ou 404).
+     * @return Une ResponseEntity avec le JWT dans le header "Authentication" si le
+     *         login s'est bien passé, et le code de statut approprié (204, 401 ou
+     *         404).
      */
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password, @RequestHeader("Origin") String origin) {
+    public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password,
+            @RequestHeader("Origin") String origin) {
         User user = userDao.get(login).orElse(null);
         if (user == null) {
             return ResponseEntity.status(404).build();
@@ -44,21 +49,49 @@ public class UsersOperationsController {
     }
 
     /**
-     * Réalise la déconnexion
+     * Logs out the user associated with the provided JWT token.
+     *
+     * @param jwt The JWT token used for authentication.
+     * @return A ResponseEntity with a status code indicating the result of the logout operation.
+     *         Returns 204 (No Content) if the logout is successful.
+     *         Returns 404 (Not Found) if the user associated with the provided JWT token is not found.
      */
-//    @PostMapping("/logout")
-    // TODO
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authentication") String jwt) {
+        String login = JwtHelper.getLogin(jwt);
+        User user = userDao.get(login).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(404).build();
+        }
+
+        user.disconnect();
+        return ResponseEntity.status(204).build();
+    }
 
     /**
-     * Méthode destinée au serveur Node pour valider l'authentification d'un utilisateur.
+     * Méthode destinée au serveur Node pour valider l'authentification d'un
+     * utilisateur.
      *
-     * @param token  Le token JWT qui se trouve dans le header "Authentication" de la requête
-     * @param origin L'origine de la requête (pour la comparer avec celle du client, stockée dans le token JWT)
+     * @param token  Le token JWT qui se trouve dans le header "Authentication" de
+     *               la requête
+     * @param origin L'origine de la requête (pour la comparer avec celle du client,
+     *               stockée dans le token JWT)
      * @return Une réponse vide avec un code de statut approprié (204, 400, 401).
      */
     @GetMapping("/authenticate")
-    public ResponseEntity<Void> authenticate(@RequestParam("jwt") String jwt, @RequestParam("origin") String origin) {
-        // TODO
-        return null;
+    public ResponseEntity<String> authenticate(@RequestParam("jwt") String jwt, @RequestParam("origin") String origin) {
+        if (jwt == null || jwt.isEmpty()) {
+            return ResponseEntity.status(400).body("JWT is missing.");
+        }
+
+        if (origin == null || origin.isEmpty()) {
+            return ResponseEntity.status(400).body("Origin is missing.");
+        }
+
+        if (JwtHelper.verifyToken(jwt, origin)) {
+            return ResponseEntity.status(204).build();
+        }
+
+        return ResponseEntity.status(401).build();
     }
 }
