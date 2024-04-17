be86af155718f012dd5fead0a893b90734a6b5fa
LeXillal <julien38.b@gmail.com>
2024-04-17 14:30:32 +0200

    fix: not used

--- users/src/main/java/fr/univlyon1/m1if/m1if13/users/config/WebConfig.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/config/WebConfig.java
@@ -3,7 +3,6 @@ package fr.univlyon1.m1if.m1if13.users.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
